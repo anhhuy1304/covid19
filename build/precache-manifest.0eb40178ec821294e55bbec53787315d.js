bed9310471f0e1664399d94647c5b310130cef68
anhhuy1304 <1653030@student.hcmus.edu.vn>
2020-05-06 18:19:12 +0700

    deploy

---
+++ build/precache-manifest.0eb40178ec821294e55bbec53787315d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5595ad10c5928f043ffe5835bc3ed77",
+    "url": "/covid19/index.html"
+  },
+  {
+    "revision": "3f507094fa0c4ec1c8f6",
+    "url": "/covid19/static/css/main.617cf1f8.chunk.css"
+  },
+  {
+    "revision": "80f4f58fac45c0e6a712",
+    "url": "/covid19/static/js/2.3753f3b8.chunk.js"
+  },
+  {
+    "revision": "461ffef3b41d3396f6b5a90d0f360455",
+    "url": "/covid19/static/js/2.3753f3b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f507094fa0c4ec1c8f6",
+    "url": "/covid19/static/js/main.87cad7ac.chunk.js"
+  },
+  {
+    "revision": "2aab8187fcbb9c9582c8",
+    "url": "/covid19/static/js/runtime-main.28c10ec6.js"
+  }
+]);
